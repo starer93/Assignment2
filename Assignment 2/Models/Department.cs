d7bd53feecb3e879191b4c9eb5986e6a61fafe77
starer93 <starer93@hotmail.com>
2014-05-12 19:11:26 +1000

    First commit
    First commit of the assignment

---
+++ Assignment 2/Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace Assignment_2.Models
+{
+    public enum DepartmentName
+    {
+        State_Services,
+        Logistics_Services,
+        Higher_Education_Services,
+
+    };
+
+    public class Department
+    {
+        private double monthlyBudget;
+        public string Name { set; get; }
+        public Department()
+        {
+            monthlyBudget = Convert.ToDouble(ConfigurationManager.AppSettings["MonthlyBudget"]);
+        }
+
+        public double GetMonthlyBudget()
+        {
+            return monthlyBudget;
+        }
+    }
+}
---
+++ Assignment 2/Models/UserAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_2.Models
+{
+    public abstract class UserAccount
+    {
+        public string UserId { protected set; get; }
+        public List<Report> Reports { protected set; get; }
+        public Department department { set; get; }
+
+        protected UserAccount(string userId)
+        {
+            UserId = userId;
+        }
+    }
+
+    public class Consultant: UserAccount
+    {
+        public Consultant(string userId) : base (userId)
+        {
+
+        }
+    }
+
+    public class Supervisor: UserAccount
+    {
+        public Supervisor(string userId) : base (userId)
+        {
+
+        }
+    }
+
+    public class AccountStaff: UserAccount
+    {
+        public AccountStaff(string userId) : base (userId)
+        {
+            department = null;
+        }
+    }
+}
